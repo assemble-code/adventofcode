bf6c71dad1ec21d28bda80827985cdaea94c248d
prabhat yadav <prabhatyadav3108@gmail.com>
2024-12-09 01:18:16 +0530

    day 8

--- src/main/java/com/adventofcode/year_2024/day8/Main.java
+++ src/main/java/com/adventofcode/year_2024/day8/Main.java
@@ -30,36 +30,42 @@ public class Main {
 
         }
 
-
-        /// calculate part one
-        part1(charPoint, data.size());
+        findAntiNode(charPoint, data.size());
     }
 
 
-    static void part1(HashMap<Character, List<Point>> charPoint, int size) {
-        int maxX = size ;
-        int maxY = size ;
+    static void findAntiNode(HashMap<Character, List<Point>> charPoint, int size) {
+        int maxX = size;
+        int maxY = size;
 
         int count = 0;
-        HashSet<Point> uniquePoints = new HashSet<>();
+        HashSet<Point> uniquePointsPart1 = new HashSet<>();
+        HashSet<Point> uniquePointsPart2 = new HashSet<>();
+
+
         for (Map.Entry<Character, List<Point>> points : charPoint.entrySet()) {
             //if single antenna no need to calculate
             if (points.getValue().size() == 1) {
                 continue;
             }
             List<Point> pointList = points.getValue();
-            for (int i = 0; i < pointList.size()-1; i++) {
+            for (int i = 0; i < pointList.size() - 1; i++) {
                 for (int j = i + 1; j < pointList.size(); j++) {
-
+                    /// part 1 calculation
                     List<Point> listOfAntiNodes = calculateValidAntiNode(pointList.get(i), pointList.get(j), maxX, maxY);
+                    uniquePointsPart1.addAll(listOfAntiNodes);
 
-                    uniquePoints.addAll(listOfAntiNodes);
+
+                    /// part 2 calculation
+                    List<Point> listOfAntiNodeWithHarmonics = calculateValidAntiNodeLocationWithHarmonics(pointList.get(i), pointList.get(j), maxX, maxY);
+                    uniquePointsPart2.addAll(listOfAntiNodeWithHarmonics);
 
                 }
 
             }
         }
-        System.out.println("Part one --> "+uniquePoints.size());
+        System.out.println("Part one --> " + uniquePointsPart1.size());
+        System.out.println("Part two --> " + uniquePointsPart2.size());
     }
 
 
@@ -69,7 +75,10 @@ public class Main {
         int difX = p2.x() - p1.x();
         int difY = p2.y() - p1.y();
 
+        ///by subtracting the diff from p1 we move twice far from p2
         Point antiNode1 = new Point(p1.x() - difX, p1.y() - difY);
+
+        /// by adding the diff we move twice far from p1
         Point antiNode2 = new Point(p2.x() + difX, p2.y() + difY);
 
         //anti nodes are within grid
@@ -81,18 +90,47 @@ public class Main {
             points.add(antiNode2);
         }
 
-
         return points;
     }
 
 
+    static List<Point> calculateValidAntiNodeLocationWithHarmonics(Point p1, Point p2, int maxX, int maxY) {
+        int difX = p2.x() - p1.x();
+        int difY = p2.y() - p1.y();
+        List<Point> points = new ArrayList<>();
+        Point antiNode1 = p1;
+        Point antiNode2 = p2;
+
+        /// location is valid add the point
+        while (validLocation(antiNode1, maxX, maxY) || validLocation(antiNode2, maxX, maxY)) {
+
+            if (validLocation(antiNode1, maxX, maxY)) {
+                points.add(antiNode1);
+            }
+            if (validLocation(antiNode2, maxX, maxY)) {
+                points.add(antiNode2);
+            }
+
+            // now update the coordinates
+            // by moving away from the last point using the diff
+            antiNode1 = new Point(antiNode1.x() - difX, antiNode1.y() - difY);
+            antiNode2 = new Point(antiNode2.x() + difX, antiNode2.y() + difY);
+        }
+
+        return points;
+    }
+
+    /**
+     * Validates that the point is within limit
+     *
+     * @param p1   Point which needs to be validated
+     * @param maxX Upperbound of X
+     * @param maxY Upperbound of Y
+     * @return {@code true } If the point is within bounds
+     */
     static boolean validLocation(Point p1, int maxX, int maxY) {
 
         return p1.x() >= 0 && p1.x() < maxX && p1.y() >= 0 && p1.y() < maxY;
     }
 
-   static int getDistance(Point a, Point b) {
-
-        return Math.abs(a.x() - b.x()) + Math.abs(a.y() - b.y());
-    }
 }
